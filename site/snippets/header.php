5e804102d4a87a6f42962bab95ab94b54ca3bd97
Timo Schneider <timo@schnti.de>
2021-10-09 21:57:21 +0200

    Removed unused.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
 <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title><?php e(!$page->isHomePage(), $page->title()->value() . ' - '); ?><?= $site->title()->value(); ?></title>
     <meta name="description" content="<?= $site->description()->value(); ?>">
