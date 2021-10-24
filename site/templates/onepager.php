9997c309503fa4e08061c8a372c194688634b2bc
Timo Schneider <timo@schnti.de>
2021-10-24 14:27:16 +0200

    Update module structure.

--- site/templates/onepager.php
+++ site/templates/onepager.php
@@ -1,5 +1,12 @@
 <?php snippet('header'); ?>
 
-<?php snippet('modules'); ?>
+<?php if ($page->hasModules()): ?>
+	<?php foreach ($page->modules() as $module) : ?>
+		<div class="anchor" id="<?= $module->uid(); ?>"></div>
+		<div class="wrapper <?= $module->moduleId(); ?> <?= $module->classes(); ?>">
+			<?php echo $module->renderModule(); ?>
+		</div>
+	<?php endforeach; ?>
+<?php endif; ?>
 
 <?php snippet('footer'); ?>
