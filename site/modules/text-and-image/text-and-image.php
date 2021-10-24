a650f1f87d2ab6692d508fdc418680d4b5408da3
Timo Schneider <timo@schnti.de>
2021-10-24 14:35:37 +0200

    Update to bootstrap v5.

--- site/modules/text-and-image/text-and-image.php
+++ site/modules/text-and-image/text-and-image.php
@@ -1,18 +1,20 @@
-<div class="container">
+<div class="container-md">
 
-    <div class="row align-items-center">
-        <div class="col-sm-7 col-md-8">
+    <?php $imageLeft = ($module->position()->value() == 'left'); ?>
+
+    <div class="row justify-content-md-center">
+        <div class="col-md-8 col-lg-4 <?php e($imageLeft, 'offset-lg-1'); ?>">
 			<?php if ($module->titleDisplay()->bool()): ?>
-                <h2><?= $module->title()->html(); ?></h2>
+                <h2><?= $module->title()->kirbytextinline(); ?></h2>
 			<?php endif; ?>
 
             <?= $module->text()->kirbytext(); ?>
         </div>
-        <div class="col-sm-5 <?php e($module->position()->value() == 'left', 'order-sm-first'); ?> col-md-4">
+        <div class="col-md-4 col-lg-5 <?php e($imageLeft, 'order-first', 'offset-lg-1'); ?>">
 
 			<?php if ($image = $module->picture()->toFile()) : ?>
                 <div class="image">
-                    <img src="<?= $image->crop(600, 600)->url(); ?>" class="img-fluid" alt=""/>
+                    <img src="<?= $image->crop(800, 600)->url(); ?>" class="img-fluid" alt=""/>
                 </div>
 			<?php endif; ?>
         </div>
